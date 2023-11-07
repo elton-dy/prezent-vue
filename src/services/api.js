53ad33ea8479dadc0f58092ba52260588c2dec78
edoisy <edoisy@relatia.fr>
2023-11-07 12:14:22 +0100

    feat:view add page home

--- src/services/api.js
+++ src/services/api.js
@@ -5,8 +5,13 @@ const apiClient = axios.create({
     withCredentials: false,
     headers: {
         'Accept': 'application/json',
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
     }
 });
+
+const token = localStorage.getItem('accessToken');
+if (token) {
+    apiClient.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+}
 
 export default apiClient;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,48 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>Welcome to the Home Page</h1>
+    <!-- Content of your home page goes here -->
+  </div>
 </template>
+
+<script>
+import apiClient from "../services/api"; // Make sure to import your configured axios instance
+
+export default {
+  name: 'HomePage',
+  data() {
+    return {
+      // other data properties if needed
+    };
+  },
+  created() {
+    this.checkVisitor();
+  },
+  methods: {
+    checkVisitor() {
+      const isConnected = !!localStorage.getItem('accessToken'); // Check if the user is logged in
+
+      if (!isConnected) {
+        // Check if visitor information already exists in localStorage
+        if (!localStorage.getItem('visitorInfo')) {
+          // No visitor info available, create a new visitor
+          this.createVisitor();
+        }
+        // If visitorInfo exists in localStorage, do nothing
+      }
+    },
+    createVisitor() {
+      apiClient.post('/visitors/')
+          .then(response => {
+            // Handle the response after creating the visitor
+            // Store the visitor information in localStorage
+            localStorage.setItem('visitorInfo', JSON.stringify(response.data));
+            // Add any other processing if necessary
+          })
+          .catch(error => {
+            console.error('There was an error creating the visitor:', error);
+          });
+    }
+  }
+};
+</script>
