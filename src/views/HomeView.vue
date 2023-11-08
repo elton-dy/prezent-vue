749173ed76414127e8f5602fb29bbea2cb97d7dd
edoisy <edoisy@relatia.fr>
2023-11-08 16:57:59 +0100

    feat:view add chat component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,57 @@
 <template>
   <div>
     <h1>Welcome to the Home Page</h1>
-    <!-- Content of your home page goes here -->
+    <div class="chat-wrapper">
+      <chat-component :messages="conversation.messages"></chat-component>
+    </div>
   </div>
 </template>
 
 <script>
 import apiClient from "../services/api";
+import ChatComponent from "../components/ChatComponent.vue";
+import {onMounted,onBeforeMount, ref} from "vue";
 
 export default {
   name: 'HomePage',
+  components: {ChatComponent},
   data() {
     return {
       // other data properties if needed
+    };
+  },
+  setup() {
+    const conversation = ref({
+      id: null,
+      messages: [],
+    });
+
+    // Function to load or start a new conversation
+    async function loadOrCreateConversation() {
+      let storedConversation = localStorage.getItem('conversation');
+      if (storedConversation) {
+        conversation.value = JSON.parse(storedConversation);
+      } else {
+        try {
+          const visitorUuid = JSON.parse(localStorage.getItem('visitorInfo')).uuid;
+          const response = await apiClient.post('/conversations/', { visitor_uuid: visitorUuid });
+          // const r = await apiClient.get('/conversations/'+response.data.id);
+          const r = await apiClient.get('/conversations/1');
+          conversation.value = {
+            ...r.data,
+          };
+
+          localStorage.setItem('conversation', JSON.stringify(conversation.value));
+        } catch (error) {
+          console.error('Error starting a new conversation:', error);
+        }
+      }
+      console.log(conversation.value);
+    }
+
+    onMounted(loadOrCreateConversation);
+    return {
+      conversation,
     };
   },
   created() {
@@ -21,7 +60,6 @@ export default {
   methods: {
     checkVisitor() {
       const isConnected = !!localStorage.getItem('accessToken'); // Check if the user is logged in
-
       if (!isConnected) {
         // Check if visitor information already exists in localStorage
         if (!localStorage.getItem('visitorInfo')) {
