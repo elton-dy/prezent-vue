828081cc421c70d44a2b21bafb83cd912b6d2846
edoisy <edoisy@relatia.fr>
2024-02-02 09:45:16 +0100

    feat: google tag

--- src/components/ChatComponent.vue
+++ src/components/ChatComponent.vue
@@ -29,7 +29,15 @@
 
         <div v-else class="product-card-container ">
           <p>{{message.text}}</p>
-          <div class="product-card border-2 border-flame-orange/50 w-64 flex flex-col bg-slate-100 rounded-lg overflow-hidden" v-for="(product, productIndex) in message.product_details" :key="`product-${productIndex}`">
+          <div class="product-card border-2 border-flame-orange/50 w-64 flex flex-col bg-slate-100 rounded-lg overflow-hidden relative" v-for="(product, productIndex) in message.product_details" :key="`product-${productIndex}`">
+            <div class="absolute top-2 left-2">
+              <button @click="toggleLike(productIndex)" class="text-flame-orange hover:text-mustard-yellow">
+                <!-- Vous pouvez utiliser une icône SVG ou une classe d'icônes (comme FontAwesome) ici -->
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                </svg>
+              </button>
+            </div>
             <div class="product-card-img-container flex">
               <img v-if="!product.image_url" class="product-card-img " src="../assets/gift-box.png">
               <img v-else class="product-card-img " :src="product.image_url">
@@ -79,7 +87,22 @@ export default {
     },
     isLoading: Boolean
   },
-
+  methods: {
+    // async handleLike(productId) {
+    //   const response = await apiClient.post('/product/', { visitor_uuid: visitorUuid ,name: 'Bienvenue' });
+    //   axios.post(`YOUR_API_ENDPOINT/likes`, {
+    //     productId: productId
+    //   })
+    //   .then(response => {
+    //     // Gérer la réponse, par exemple, mettre à jour le nombre de likes dans l'état du composant
+    //     console.log(response.data);
+    //     // Mettez à jour l'état de votre composant ici
+    //   })
+    //   .catch(error => {
+    //     console.error("There was an error registering the like: ", error);
+    //   });
+    // }
+  },
   setup(props) {
     const lastMessageRef = ref(null);
 
